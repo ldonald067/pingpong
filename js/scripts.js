e4876975e1184fd75dc215520c4304dda9ec3a7b
LaTanya Donaldson <ldonald234@gmail.com>
2017-06-23 08:33:40 -0700

    Add business logic for calculation with branching and empty array so results can be pushed into the array

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+var pingPong = function(input) {
+var total = [];
+for (var number = 1; number <= index; number ++) {
+
+if (number % 15 === 0) {
+  total.push("pingpong");
+}
+
+
+else if (number % 5 === 0) {
+  total.push("pingpong");
+}
+
+
+else if (number % 3 === 0) {
+  total.push("pingpong");
+}
+
+
+else {
+  total.push(index);
+}
+
+}
+
+return total;
+
+}
