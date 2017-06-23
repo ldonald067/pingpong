de78d62b62555a9c0118874e24e230249d137f84
LaTanya Donaldson <ldonald234@gmail.com>
2017-06-23 09:48:50 -0700

    Add console.log to backend logic. Add submit and foreach function to frontend logic. Add variable for userinput and newtotals to frontend logic. Append newtotal

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,49 @@
 //Business logic//
 
 var pingPong = function(input) {
-var total = [];
-for (var number = 1; number <= input; number ++) {
+  var total = [];
+  for (var number = 1; number <= input; number ++) {
 
-if (number % 15 === 0) {
-  total.push("pingpong");
+    if (number % 15 === 0) {
+      total.push("pingpong");
+      console.log(total);
+    }
+
+
+    else if (number % 5 === 0) {
+      total.push("pong");
+      console.log(total);
+    }
+
+
+    else if (number % 3 === 0) {
+      total.push("ping");
+      console.log(total);
+    }
+
+
+    else {
+      total.push(number);
+      console.log(total);
+
+    }
+  }
+  
+  console.log(total);
+  return total;
+
 }
 
+//Front end logic//
 
-else if (number % 5 === 0) {
-  total.push("pong");
-}
+$(document).ready(function() {
+  $("form.form-inline").submit(function(event) {
+    event.preventDefault();
+    var userInput = parseInt($("input#pong").val());
+    var newTotals = pingPong(userInput);
+    newTotals.forEach(function(newTotal) {
+      $(".list").append("<li>" + newTotal + "</li>");
 
-
-else if (number % 3 === 0) {
-  total.push("ping");
-}
-
-
-else {
-  total.push(input);
-}
-
-}
-console.log(total);
-return total;
-
-}
+    });
+  });
+});
